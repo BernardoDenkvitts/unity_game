4571b22607a3c6c063350f12311679f48102669b
Bernardo Arcari Denkvitts <Bernardoarcari@gmail.com>
2024-11-15 11:01:35 -0300

    feat: script para gerar os obstáculos pela pista

---
+++ Assets/Scripts/Pista.cs
@@ -0,0 +1,56 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Pista : MonoBehaviour
+{
+
+    public GameObject[] obstacles;
+
+    public Vector2 numberOfObstacles;
+    
+    public List<GameObject> newObstacles;
+    
+    void Start()
+    {
+        // Gera um número aleatório de obstáculos dentro do intervalo definido
+        int newNumberOfObstacles = (int)Random.Range(numberOfObstacles.x, numberOfObstacles.y);
+        Debug.Log($"Número de obstáculos a serem gerados: {newNumberOfObstacles}");
+        
+        // Instancia o número determinado de obstáculos e os adiciona à lista newObstacles
+        for (int i = 0; i < newNumberOfObstacles; i++)
+        {
+            newObstacles.Add(Instantiate(obstacles[Random.Range(0, obstacles.Length)], transform));
+            newObstacles[i].SetActive(false);
+        }
+        
+        PositionateObstacle();
+    }
+
+    void PositionateObstacle()
+    {
+        
+        GameObject obstacle = obstacles[Random.Range(0, obstacles.Length)];
+        for (int i = 0; i < newObstacles.Count; i++)
+        {
+            // 334.9f é o tamanho da pista
+
+            // Define a posição mínima e máxima para posicionar o obstáculo no eixo Z
+            // Serve para distribuir os obstáculos pela pista
+            // + 6 evita que objetos nasçam muito próximo um do outro
+            float zMinPosition = ((334.9f / newObstacles.Count) * 2) * i + 6;
+            float zMaxPosition = zMinPosition + 1;
+            
+            Vector3 newPosition = new Vector3(0, 0, Random.Range(zMinPosition, zMaxPosition));
+            
+            // Define a nova posição do obstáculo no espaço local
+            newObstacles[i].transform.localPosition = newPosition;
+            
+            // Ativa o obstáculo
+            newObstacles[i].SetActive(true);
+            
+            Debug.Log($"Obstáculo {i} posicionado em {newPosition}");
+            
+        }
+    }
+}
